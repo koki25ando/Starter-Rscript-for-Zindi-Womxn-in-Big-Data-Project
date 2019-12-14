057ebf2a0edf44dd18e359082c6aec0111bd3192
koki25ando <basket.koki.bowl@gmail.com>
2019-12-14 10:56:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Starter Rscript
+
+[Zindi Womxn in Big Data South Africa](https://zindi.africa/competitions/womxn-in-big-data-south-africa-female-headed-households-in-south-africa)
+
---
+++ lgbm_starter.R
@@ -0,0 +1,35 @@
+# Preparation: importing packages and data
+pacman::p_load(tidyverse, data.table, Metrics, lightgbm)
+sample_submission = fread("Data/SampleSubmission.csv")
+train = fread("Data/Train.csv")
+test = fread("Data/Test.csv")
+
+test$target = NA # add target column to test data before merging data
+merged_dat = as.data.frame(rbind(train, test)) 
+
+# Label Encoding
+char_vars = names(select_if(merged_dat, is.character))
+merged_dat[, char_vars] = apply(merged_dat[, char_vars], 2, function(x) as.numeric(as.factor(x))-1 )
+
+
+# Data Split
+train_dat = merged_dat[!is.na(merged_dat$target), ]
+test_dat =  merged_dat[is.na(merged_dat$target), ]
+
+# LightGBM Modeling
+lgbm_train = lgb.Dataset(data = as.matrix(select(train_dat, -target)), label = train_dat$target)
+lbgm_test = data.matrix(select(test_dat, -target))
+lgb.model <- lgb.train(params = params,
+                       data = lgbm_train,
+                       nrounds=5000, 
+                       verbose=1,
+                       eval_freq=100)
+lgbm_pred = predict(lgb.model, as.matrix(lbgm_test)) 
+
+
+# Make & export a submission file
+sample_submission$target = lgbm_pred
+write.csv(sample_submission, "submission.csv", row.names = F)
+
+
+
